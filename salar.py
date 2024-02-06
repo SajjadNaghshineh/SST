bc9f0a80e1140368b8a73e1c991e22ba8580e915
Sajjad <99192892+SajjadNaghshineh@users.noreply.github.com>
2024-02-06 13:33:40 +0330

    last change last chance

--- salar.py
+++ salar.py
@@ -9,13 +9,15 @@ import datetime as dt
 from utils import set_period
 import info
 
-symbol = "GBPUSD"
-timeframe = "M1"
+symbol = "EURUSD"
+timeframe = "M3"
 bars = 30000
 # bars = 42000
-min_rsi = 15
-max_rsi = 85
+min_rsi = 30
+max_rsi = 70
 rr = 2
+balance = 10000
+risk = 0.3
 
 def run_server(username, password, server, path):
     first_connection = mt5.initialize(
@@ -60,6 +62,27 @@ def find_positions(df, min_rsi, max_rsi):
     
     return df
 
+def stop_loss_condition(df, symbol):
+    last_atr = df.iloc[-1]['atr']
+    
+    if symbol == "XAUUSD":
+        if last_atr < 0.2:
+            allowed = False
+        else:
+            allowed = True
+    elif "JPY" in symbol:
+        if last_atr < 0.002:
+            allowed = False
+        else:
+            allowed = True
+    else:
+        if last_atr < 0.0002:
+            allowed = False
+        else:
+            allowed = True
+            
+    return allowed
+
 def tp_sl_calculation(df, order_type, rr):
     last_candle = df.iloc[-1]
     
@@ -72,14 +95,14 @@ def tp_sl_calculation(df, order_type, rr):
         
     return round(tp, 5), round(sl, 5)
 
-def volume_calculation(df, symbol):
-    user_info = mt5.account_info()
-    balance = user_info[10]
-    one_percent = int(balance / 100) * 0.2
+def volume_calculation(df, symbol, balance, risk):
+    one_percent = int(balance / 100) * risk
     
     last_atr = df.iloc[-1]['atr']
     if 'JPY' in symbol:
         last_atr = last_atr * 1000
+    elif symbol == "XAUUSD":
+        last_atr = last_atr * 100
     else:
         last_atr = last_atr * 10000
         
@@ -119,7 +142,7 @@ def place_order(symbol, order_type, tp, sl, volume):
     result = mt5.order_send(request)
     order_status = result.retcode
     
-    return order_status
+    return order_status, price
 
 print(connection := run_server(info.USERNAME, info.PASSWORD, info.SERVER, info.PATH))
 if not connection[0] or not connection[1]:
@@ -129,41 +152,52 @@ if not connection[0] or not connection[1]:
 while True:
     try:
         today = dt.datetime.today()
+        current_time = dt.datetime.now().time()
         if today.weekday() not in [5, 6]:
-            df = retrieve_data(symbol, timeframe, bars)
-            df = add_indicators(df)
-            df = find_positions(df, min_rsi, max_rsi)
-            
-            if df.iloc[-1]['buy'] == 1:
-                find_time = dt.datetime.now().replace(microsecond=0)
-                print(f"Found a Buy position for {symbol} at {find_time}")
+            if current_time >= dt.time(0, 0) and current_time >= dt.time(4, 30):
+                connection = run_server(info.USERNAME, info.PASSWORD, info.SERVER, info.PATH)
+                if not connection[0] or not connection[1]:
+                    print(dt.datetime.now().replace(microsecond=0))
+                    raise ValueError("Couldn't connect to server")
                 
-                tp, sl = tp_sl_calculation(df, 'buy', rr)
-                volume = volume_calculation(df, symbol)
-                order_status = place_order(symbol, "buy", tp, sl, volume)
+                df = retrieve_data(symbol, timeframe, bars)
+                df = add_indicators(df)
+                df = find_positions(df, min_rsi, max_rsi)
                 
-                if order_status != mt5.TRADE_RETCODE_DONE:
-                    error_time = dt.datetime.now().replace(microsecond=0)
-                    print(f"Trade operation has an error: {order_status} at {error_time}")
-                else:
-                    trade_time = dt.datetime.now().replace(microsecond=0)
-                    print(f"Buy for {symbol} at {trade_time}.")
-            elif df.iloc[-1]['sell'] == 1:
-                find_time = dt.datetime.now().replace(microsecond=0)
-                print(f"Found a Sell position for {symbol} at {find_time}")
-                
-                tp, sl = tp_sl_calculation(df, 'sell', rr)
-                volume = volume_calculation(df, symbol)
-                order_status = place_order(symbol, "sell", tp, sl, volume)
-                
-                if order_status != mt5.TRADE_RETCODE_DONE:
-                    error_time = dt.datetime.now().replace(microsecond=0)
-                    print(f"Trade operation has an error: {order_status} at {error_time}")
-                else:
-                    trade_time = dt.datetime.now().replace(microsecond=0)
-                    print(f"Sell for {symbol} at {trade_time}.")
+                if df.iloc[-1]['buy'] == 1:
+                    allowed = stop_loss_condition(df, symbol)
+                    if allowed:
+                        find_time = dt.datetime.now().replace(microsecond=0)
+                        print(f"Found a Buy position for {symbol} at {find_time}")
+                        
+                        tp, sl = tp_sl_calculation(df, 'buy', rr)
+                        volume = volume_calculation(df, symbol, balance, risk)
+                        order_status, price = place_order(symbol, "buy", tp, sl, volume)
+                        
+                        if order_status != mt5.TRADE_RETCODE_DONE:
+                            error_time = dt.datetime.now().replace(microsecond=0)
+                            print(f"Trade operation has an error: {order_status} at {error_time}")
+                        else:
+                            trade_time = dt.datetime.now().replace(microsecond=0)
+                            print(f"Buy for {symbol} at {trade_time}, sl:{sl}, calc entry: {df.iloc[-1]['close']}, real entry: {price}, tp: {tp}")
+                elif df.iloc[-1]['sell'] == 1:
+                    allowed = stop_loss_condition(df, symbol)
+                    if allowed:
+                        find_time = dt.datetime.now().replace(microsecond=0)
+                        print(f"Found a Sell position for {symbol} at {find_time}")
+                        
+                        tp, sl = tp_sl_calculation(df, 'sell', rr)
+                        volume = volume_calculation(df, symbol, balance, risk)
+                        order_status, price = place_order(symbol, "sell", tp, sl, volume)
+                        
+                        if order_status != mt5.TRADE_RETCODE_DONE:
+                            error_time = dt.datetime.now().replace(microsecond=0)
+                            print(f"Trade operation has an error: {order_status} at {error_time}")
+                        else:
+                            trade_time = dt.datetime.now().replace(microsecond=0)
+                            print(f"Sell for {symbol} at {trade_time}, sl:{sl}, calc entry: {df.iloc[-1]['close']}, real entry: {price}, tp: {tp}")
         time.sleep(60)
     except Exception as e:
         now = dt.datetime.now().replace(microsecond=0)
         print(f"Error: {e} at {now}")
-        
+        break
